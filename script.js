4c928bcdace8137ed740d0aafb416dffc0495bce
Suhad Mia <suhadmia12@gmail.com>
2022-04-16 22:47:31 +0600

    some add style

---
+++ script.js
@@ -0,0 +1,12 @@
+var map;
+
+function initMap() {
+
+    // Create map
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 	23.777176, lng: 90.399452},
+        zoom: 8
+    });
+
+   
+}
